4f69ab30c1f7b9d9184822aea9e0e21f99b46f0e
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2025-01-16 21:37:51 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -22,18 +22,18 @@ function countProps(obj) {
  return obj.length
 }
 console.log(countProps(obj))
-// const employees = {
-//     Petro: 3,
-//     Sanya:5,
-//     Sergiy: 7,
-//     Vladuslav: 6
-// }
+const employees = {
+    Petro: 3,
+    Sanya:5,
+    Sergiy: 7,
+    Vladuslav: 6
+}
 
-// function findBestEmployee(employees, mostProductive) {
-//     const mostProductive = Math.max(...Object.values(employees))
-//     return mostProductive
-// }
-// console.log(findBestEmployee(employees, mostProductive))
+function findBestEmployee(employees, mostProductive) {
+    const mostProductive = Math.max(...Object.values(employees))
+    return mostProductive
+}
+console.log(findBestEmployee(employees, mostProductive))
 
 
 
@@ -53,3 +53,33 @@ function countTotalSalary(salary) {
     return total
 }
 console.log('Сума зарплат:', countTotalSalary(salary))
+const allProducts = [
+  {name:'banana', price: 5, quantity: 3},
+  {name:'apple', price: 10, quantity: 1},
+  {name:'mango', price: 15, quantity: 2}
+];
+
+function calculateTotalPrice(allProducts, productName) {
+    let total = 0;
+    for (const product of allProducts) {
+        if (product.name === productName) {
+            total += product.price * product.quantity;
+        }
+    }
+    return total;
+}
+
+console.log(calculateTotalPrice(allProducts, 'banana'));
+function getAllPropValues(arr, prop) {
+const values = [];
+for (const item of arr) {
+    if (prop in item) {
+        values.push(item[prop]);
+    }
+}
+return values;
+}
+
+console.log(getAllPropValues(allProducts, 'name')); 
+console.log(getAllPropValues(allProducts, 'price'));
+console.log(getAllPropValues(allProducts, 'quantity'));
